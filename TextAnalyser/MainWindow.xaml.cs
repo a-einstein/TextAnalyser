ba96e48c810658c3890ac45109641ace2d22776d
a-einstein <aeinstein@xs4all.nl>
2022-08-01 18:41:11 +0200

    Provisionally read file and edit text.

--- TextAnalyser/MainWindow.xaml.cs
+++ TextAnalyser/MainWindow.xaml.cs
@@ -1,28 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TextAnalyser.Resources;
+using Utilities;
 
 namespace TextAnalyser
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private ICommand? readFileCommand;
+        public ICommand ReadFileCommand => readFileCommand ?? (readFileCommand = new CommandHandler(ReadFile, () => true));
+      
+        private bool fileRead;
+        private bool FileRead
+        {
+            get { return fileRead; }
+            set
+            {
+                fileRead = value;
+            }
+        }
+
+        private void ReadFile()
+        {
+            string fileMessage; ;
+
+            FileRead = Read(out fileMessage);
+            FileMessage = fileMessage;
+        }
+
+        public bool Read(out string message)
+        {
+            var initialDirectory = Path.GetFullPath(Directory.GetCurrentDirectory() + "\\..\\..\\..\\..\\..\\Texts");
+
+            var fileDialog = new OpenFileDialog
+            {
+                Title = Texts.FileReadTitle,
+                Filter = "TXT files|*.txt",
+                InitialDirectory = initialDirectory
+            };
+
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var filePath = fileDialog.FileName;
+
+                message = string.Format(Texts.MessageFileName_name, Path.GetFileName(filePath));
+
+                Text = File.ReadAllText(filePath);
+
+                return true;
+            }
+            else
+            {
+                message = "No file read.";
+                return false;
+            }
+        }
+
+        public static readonly DependencyProperty FileMessageProperty =
+            DependencyProperty.Register(nameof(FileMessage), typeof(string), typeof(MainWindow), new PropertyMetadata("No file yet"));
+
+        public string FileMessage
+        {
+            get { return (string)GetValue(FileMessageProperty); }
+            set { SetValue(FileMessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(MainWindow), new PropertyMetadata("Read a file or edit."));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
         }
     }
 }
---
+++ TextAnalyser/Utilities/CommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Utilities
+{
+    public class CommandHandler : ICommand
+    {
+        private Action action;
+        private Func<bool> canExecute;
+
+        public CommandHandler(Action action, Func<bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        // TODO Check nullability reports.
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
